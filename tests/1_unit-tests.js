37fef14dfcc947d2f53c67f3e66a307c56dab416
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-26 21:21:38 +0000

    Add test 'Logic handles a puzzle string that is not 81 characters in length'.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,6 +6,10 @@ const Solver = require('../controllers/sudoku-solver.js');
 const solver = new Solver();
 
 const validPlacementString = '123456789.';
+const randomValidPlacement = () => {
+  const index = Math.floor(Math.random() * validPlacementString.length);
+  return validPlacementString[index];
+}
 
 const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js');
 
@@ -26,15 +30,29 @@ suite('UnitTests', () => {
       let modifiedPuzzleString = puzzleAndSolution[0];
       validPlacementString.split('').forEach(validPlacement => {
         if (puzzleAndSolution[0].indexOf(validPlacement) !== -1) {
-          modifiedPuzzleString = puzzleAndSolution[0].replace(validPlacement, String.fromCharCode(validPlacement.charCodeAt(0) + 9 + Math.ceil(Math.random() * 10)))
+          modifiedPuzzleString = puzzleAndSolution[0].replace(validPlacement, String.fromCharCode(validPlacement.charCodeAt(0) + 12 + Math.ceil(Math.random() * 10)))
         }
       })
       // console.log(`modifiedPuzzleString`, modifiedPuzzleString); // DEBUG
-      assert.isFalse(solver.validate(modifiedPuzzleString));
+      assert.isFalse(solver.validate(modifiedPuzzleString), modifiedPuzzleString);
     })
 
     done();
   });
 
+  // #3
+  test('Logic handles a puzzle string that is not 81 characters in length', done => {
+    let i = 1;
+    while (i < 100) {
+      let puzzleString = randomValidPlacement().repeat(i);
+      if (i !== 81) {
+        assert.isFalse(solver.validate(puzzleString));
+      } else {
+        assert.isTrue(solver.validate(puzzleString));
+      }
+      i++;
+    }
+    done();
+  });
 
 });
