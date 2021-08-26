5bd00d4b7442e7ec48dc660432e5c043b49d6129
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-26 20:49:49 +0000

    * Basic functionalities of controllers/sudoku-solver.js * Add test 'Logic handles a valid puzzle string of 81 characters'.

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,18 +1,80 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    return /^[1-9\.]{81}$/.test(puzzleString);
+  }
+
+  //
+  rowNumber(row) {
+    const rowString = 'ABCDEFGHI';
+    return rowString.indexOf(row);
+  }
+
+  //
+  columnNumber(column) {
+    return column - 1;
+  }
+
+  startNumber(rowOrColumnNumber) {
+    if (rowOrColumnNumber >= 0 && rowOrColumnNumber < 3) {
+      return 0;
+    } else if (rowOrColumnNumber >= 3 && rowOrColumnNumber < 6) {
+      return 3;
+    }
+    return 6;
+  }
+
+  //
+  withoutDuplicate(partString, value) {
+    const firstIndex = partString.indexOf(value);
+    if (firstIndex === -1) {
+      return true;
+    } else if (firstIndex === partString.lastIndexOf(value)) {
+      return true;
+    }
+    return false;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    if (!this.valid(puzzleString)) throw new Error('Invalid puzzle string.');
 
+    const rowNumber = this.rowNumber(row);
+    const rowStart = rowNumber * 9;
+    const rowString = puzzleString.substring(rowStart, 9);
+
+    return this.withoutDuplicate(rowString, value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    if (!this.valid(puzzleString)) throw new Error('Invalid puzzle string.');
 
+    const columnNumber = this.columnNumber(column);
+    let columnString = puzzleString[columnNumber];
+    let i = 1;
+    while (columnString.length < 9) {
+      columnString.concat(puzzleString[columnNumber * i]);
+      i++;
+    }
+
+    return this.withoutDuplicate(columnString, value);
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    if (!this.valid(puzzleString)) throw new Error('Invalid puzzle string.');
+    
+    const rowNumber = this.rowNumber(row);
+    const rowStart = this.startNumber(rowNumber);
+    const columnNumber = this.columnNumber(column);
+    const columnStart = this.startNumber(columnNumber);
+    let regionString = '';
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const index = (rowStart + i) * 9 + (columnStart + j);
+        regionString.concat(puzzleString[index]);
+      }
+    }
 
+    return this.withoutDuplicate(regionString, value);
   }
 
   solve(puzzleString) {
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,24 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+// let solver;
+const solver = new Solver();
+
+const validPlacement = '123456789.';
+
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js');
 
 suite('UnitTests', () => {
+  // #1
+  test('Logic handles a valid puzzle string of 81 characters', done => {
+
+    puzzlesAndSolutions.forEach(puzzleAndSolution => {
+      assert.isTrue(solver.validate(puzzleAndSolution[0]));
+    })
+
+    done();
+
+  });
+
 
 });
